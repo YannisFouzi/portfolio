8f9d94e6779fd6f6fdd2c28be5f4ac76f388da6f
YannisFouzi <yfouzi.dev@gmail.com>
2024-09-08 08:13:58 +0200

    Google Analytics bouton header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { Back } from 'gsap';
 import myCV from '../pdf/Yannis_Fouzi_CV_2024.pdf';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
+import ReactGA from 'react-ga4';  // Import GA4
 
 function Header() {
   const [copied, setCopied] = useState(false);
+
   useEffect(() => {
     const title = document.querySelector('.title');
     title.innerHTML = title.textContent.replace(/(\S)/g, "<span>$1</span>");
@@ -15,18 +17,31 @@ function Header() {
     // Animation : les lettres apparaissent par le bas
     gsap.fromTo(
       ".title span", 
-      { opacity: 0, bottom: -80 },  // Position de départ : 80px en dessous de la position finale
-      { opacity: 1, bottom: 0, ease: Back.easeOut.config(1.7), stagger: 0.05, duration: 0.5 }  // Position finale : position originale
+      { opacity: 0, bottom: -80 },
+      { opacity: 1, bottom: 0, ease: Back.easeOut.config(1.7), stagger: 0.05, duration: 0.5 }
     );
   }, []);
 
+  const handleCVClick = () => {
+    ReactGA.event({
+      category: 'CV',
+      action: 'Download',
+      label: 'User clicked to download CV'
+    });
+  };
+
   const handleCopy = () => {
     navigator.clipboard.writeText('yfouzi.dev@gmail.com').then(() => {
-      setCopied(true);
+      ReactGA.event({
+        category: 'Email',
+        action: 'Copy',
+        label: 'User clicked to copy email'
+      });
 
+      setCopied(true);
       setTimeout(() => {
         setCopied(false);
-      }, 1500); // Réinitialise le message après 1.5 secondes
+      }, 1500);
     });
   };
 
@@ -37,7 +52,7 @@ function Header() {
           <span className="title">Yannis Fouzi</span>
         </h1>
         <h2>Développeur React / Chef de projet</h2>
-        <a href={myCV} download className="link-github">
+        <a href={myCV} download className="link-github" onClick={handleCVClick}>
           <span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" focusable="false">
               <path d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"></path>
@@ -48,7 +63,7 @@ function Header() {
         <div className="email-container" style={{ marginTop: '20px' }}>
           <a className="link-github" onClick={handleCopy} href="#">
             <span>
-            <FontAwesomeIcon icon={faEnvelope} />
+              <FontAwesomeIcon icon={faEnvelope} />
             </span>
             {copied ? "Email copié !" : "Copiez mon email"}
           </a>
